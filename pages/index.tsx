ff29db3f9068b8f581be0a509e8dacd29bf136a1
Jorge Tapia <admin@maaide.com>
2023-06-29 11:16:18 -0400

    actualization general

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { LeftMenu } from '@/components/ui'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,7 +9,34 @@ export default function Home() {
         <title>Inicio</title>
       </Head>
       <LeftMenu>
-        <p>Inicio</p>
+        <div className='p-6 bg-[#f6f6f7] flex flex-col gap-4 dark:bg-neutral-900' style={{ width: 'calc(100% - 252px)', overflow: 'overlay' }}>
+          <div className='flex justify-between w-full max-w-1280 mx-auto mb-4'>
+            <h1 className='text-xl'>Inicio</h1>
+            <Link className='pt-1.5 pb-1.5 h-fit pl-7 pr-7 rounded-md bg-main text-white' href='/ventas/nueva-venta'>Nueva venta</Link>
+          </div>
+          <div className='flex flex-col gap-6 w-full max-w-1280 mx-auto mb-4'>
+            <h2 className='text-2xl'>¡Hola Jorge! Te damos la bienvenida al panel de control de tu tienda</h2>
+            <div className='bg-white flex flex-col gap-2 p-4 w-fit rounded-md shadow-md'>
+              <h3 className='text-lg'>Configura tu tienda</h3>
+              <div className='flex justify-between gap-4 border-t pt-4 pb-2'>
+                <p className='my-auto'>Agrega tu primer producto</p>
+                <button className='pt-1.5 pb-1.5 h-fit pl-7 pr-7 rounded-md bg-main text-white'>Agregar nuevo producto</button>
+              </div>
+              <div className='flex justify-between gap-4 border-t pt-4 pb-2'>
+                <p className='my-auto'>Agrega dominio personalizado</p>
+                <button className='pt-1.5 pb-1.5 h-fit pl-7 pr-7 rounded-md bg-main text-white'>Agregar dominio personalizado</button>
+              </div>
+              <div className='flex justify-between gap-4 border-t pt-4 pb-2'>
+                <p className='my-auto'>Configura tus metodos de pagos</p>
+                <button className='pt-1.5 pb-1.5 h-fit pl-7 pr-7 rounded-md bg-main text-white'>Configurar metodos de pagos</button>
+              </div>
+              <div className='flex justify-between gap-4 border-t pt-4 pb-2'>
+                <p className='my-auto'>Configura tus metodos de envíos</p>
+                <button className='pt-1.5 pb-1.5 h-fit pl-7 pr-7 rounded-md bg-main text-white'>Configurar metodos de envíos</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </LeftMenu>
     </>
   )
