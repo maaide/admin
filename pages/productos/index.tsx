012fea9444474778aa659d814ea032fbe868b81b
Jorge Tapia <admin@maaide.com>
2023-07-17 10:54:44 -0400

    actualization general

--- pages/productos/index.tsx
+++ pages/productos/index.tsx
@@ -34,59 +34,63 @@ const Products = () => {
                     </div>
                   </div>
                 )
-                : <table className='shadow-md w-full border dark:border-neutral-600'>
-                  <thead className='bg-white border-b w-full dark:bg-neutral-800 dark:border-neutral-600'>
-                    <tr>
-                      <th className='text-left p-2 font-normal'>Producto</th>
-                      <th className='text-left p-2 font-normal'>Precio</th>
-                      <th className='text-left p-2 font-normal'>Estado</th>
-                      <th className='text-left p-2 font-normal'>Stock</th>
-                      <th className='text-left p-2 font-normal'>Categoria</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody className='bg-white w-full dark:bg-neutral-800 dark:border-neutral-600'>
-                    {
-                      products.map(product => (
-                        <tr className='border-b cursor-pointer w-full dark:border-neutral-600 hover:bg-neutral-100 dark:hover:bg-neutral-700' key={product._id}>
-                          <td className='flex gap-2 p-2' style={{width: '100%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
-                            <img className='w-20' src={product.images[0]} />
-                            <div className='mt-auto mb-auto w-full'>
-                              <p className='font-light'>{product.name}</p>
-                            </div>
-                          </td>
-                          <td className='p-2' style={{width: '15%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
-                            <p className='font-light'>${NumberFormat(product.price)}</p>
-                            {
-                              product.beforePrice
-                                ? <p className='font-light text-sm line-through'>${NumberFormat(product.beforePrice)}</p>
-                                : ''
-                            }
-                          </td>
-                          <td className='p-2' style={{width: '15%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
-                            {
-                              product.state === true
-                                ? <p className='font-light w-fit pt-1 pb-1 pl-2 pr-2 bg-green-500 rounded-md text-white'>Activo</p>
-                                : <p className='font-light w-fit pt-1 pb-1 pl-2 pr-2 bg-red-500 rounded-md text-white'>Borrador</p>
-                            }
-                          </td>
-                          <td className='p-2' style={{width: '10%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
-                            <p className='font-light'>{product.stock}</p>
-                          </td>
-                          <td className='p-2' style={{width: '20%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
-                            <p className='font-light'>{product.category}</p>
-                          </td>
-                          <td className='p-2'>
-                            <button onClick={async(e: any) => {
-                              e.preventDefault()
-                              await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/products/${product._id}`)
-                            }}><AiOutlineClose /></button>
-                          </td>
+                : products.length
+                  ? (
+                    <table className='shadow-md w-full border dark:border-neutral-600'>
+                      <thead className='bg-white border-b w-full dark:bg-neutral-800 dark:border-neutral-600'>
+                        <tr>
+                          <th className='text-left p-2 font-normal'>Producto</th>
+                          <th className='text-left p-2 font-normal'>Precio</th>
+                          <th className='text-left p-2 font-normal'>Estado</th>
+                          <th className='text-left p-2 font-normal'>Stock</th>
+                          <th className='text-left p-2 font-normal'>Categoria</th>
+                          <th></th>
                         </tr>
-                      ))
-                    }
-                  </tbody>
-                </table>
+                      </thead>
+                      <tbody className='bg-white w-full dark:bg-neutral-800 dark:border-neutral-600'>
+                        {
+                          products.map(product => (
+                            <tr className='border-b cursor-pointer w-full dark:border-neutral-600 hover:bg-neutral-100 dark:hover:bg-neutral-700' key={product._id}>
+                              <td className='flex gap-2 p-2' style={{width: '100%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
+                                <img className='w-20' src={product.images[0]} />
+                                <div className='mt-auto mb-auto w-full'>
+                                  <p className='font-light'>{product.name}</p>
+                                </div>
+                              </td>
+                              <td className='p-2' style={{width: '15%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
+                                <p className='font-light'>${NumberFormat(product.price)}</p>
+                                {
+                                  product.beforePrice
+                                    ? <p className='font-light text-sm line-through'>${NumberFormat(product.beforePrice)}</p>
+                                    : ''
+                                }
+                              </td>
+                              <td className='p-2' style={{width: '15%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
+                                {
+                                  product.state === true
+                                    ? <p className='font-light w-fit pt-1 pb-1 pl-2 pr-2 bg-green-500 rounded-md text-white'>Activo</p>
+                                    : <p className='font-light w-fit pt-1 pb-1 pl-2 pr-2 bg-red-500 rounded-md text-white'>Borrador</p>
+                                }
+                              </td>
+                              <td className='p-2' style={{width: '10%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
+                                <p className='font-light'>{product.stock}</p>
+                              </td>
+                              <td className='p-2' style={{width: '20%'}} onClick={() => router.push(`/productos/${product.slug}`)}>
+                                <p className='font-light'>{product.category}</p>
+                              </td>
+                              <td className='p-2'>
+                                <button onClick={async(e: any) => {
+                                  e.preventDefault()
+                                  await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/products/${product._id}`)
+                                }}><AiOutlineClose /></button>
+                              </td>
+                            </tr>
+                          ))
+                        }
+                      </tbody>
+                    </table>
+                  )
+                  : 'No hay productos'
             }
           </div>
         </div>
