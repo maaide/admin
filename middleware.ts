358f67e383fd61b091ce5d2622306d145390afe1
Jorge Tapia <admin@maaide.com>
2023-07-24 19:52:55 -0400

    actualization general

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-    matcher: ['/clientes/:path*', '/configuracion/:path*', '/diseno/:path*', '/estadisticas/:path*', '/marketing/:path*', '/mensajes/:path*', '/notificaciones/:path*', '/productos/:path*', '/ventas/:path*', '/']
+    matcher: ['/clientes/:path*', '/configuracion/:path*', '/diseno/:path*', '/estadisticas/:path*', '/marketing/:path*', '/mensajes/:path*', '/notificaciones/:path*', '/productos/:path*', '/ventas/:path*']
 }
