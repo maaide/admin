fb8804ef75e2852598a170f8965a0964d481d526
Jorge Tapia <admin@maaide.com>
2023-08-07 14:35:22 -0400

    actualization gneral

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,4 +1,4 @@
-import { LeftMenu, Spinner2 } from '@/components/ui'
+import { LeftMenu, Spinner, Spinner2 } from '@/components/ui'
 import { IPost } from '@/interfaces'
 import axios from 'axios'
 import Head from 'next/head'
@@ -68,124 +68,136 @@ const PostPage = () => {
             </div>
           </div>
         </div>
-        <div className='p-6 mb-16 bg-[#f6f6f7] dark:bg-neutral-900' style={{ width: 'calc(100% - 252px)', overflow: 'overlay' }}>
-          <div className='flex gap-2 w-full max-w-1280 m-auto mb-4'>
-            <Link href='/blog' className='border rounded p-2 bg-white hover:bg-neutral-50 dark:bg-neutral-800 dark:border-neutral-600'><BiArrowBack className='text-xl' /></Link>
-            <h1 className='text-xl my-auto'>Post {contentData._id}</h1>
-          </div>
-          <form onSubmit={handleSubmit} className='flex gap-4 max-w-1280 m-auto'>
-            <div className='flex gap-4 flex-col w-2/3'>
-              <div className='bg-white border flex flex-col gap-4 border-white p-4 rounded-md shadow dark:bg-neutral-800 dark:border-neutral-700'>
-                <div className='flex flex-col gap-2'>
-                  <h2>Titulo</h2>
-                  <input type='text' placeholder='Titulo del post' onChange={(e: any) => setContentData({ ...contentData, title: e.target.value })} value={contentData.title} className='font-light p-1.5 rounded border text-sm w-full focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
-                </div>
-                <div className='flex flex-col gap-2'>
-                  <h2>Contenido</h2>
-                  {
-                    contentData.content.map((content, index) => (
-                      <div key={index} className='flex gap-2'>
-                        <select value={content.type} onChange={(e: any) => {
-                          let mod: any = contentData.content
-                          mod[index].type = e.target.value
-                          setContentData({ ...contentData, content: mod })
-                        }} className='p-1.5 rounded border text-sm font-light focus:outline-none h-fit my-auto focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600'>
-                          <option>Texto</option>
-                          <option>Imagen</option>
+        {
+          contentData._id !== ''
+            ? (
+                <div className='p-6 mb-16 bg-[#f6f6f7] dark:bg-neutral-900' style={{ width: 'calc(100% - 252px)', overflow: 'overlay' }}>
+                  <div className='flex gap-2 w-full max-w-1280 m-auto mb-4'>
+                    <Link href='/blog' className='border rounded p-2 bg-white hover:bg-neutral-50 dark:bg-neutral-800 dark:border-neutral-600'><BiArrowBack className='text-xl' /></Link>
+                    <h1 className='text-xl my-auto'>Post {contentData._id}</h1>
+                  </div>
+                  <form onSubmit={handleSubmit} className='flex gap-4 max-w-1280 m-auto'>
+                    <div className='flex gap-4 flex-col w-2/3'>
+                      <div className='bg-white border flex flex-col gap-4 border-white p-4 rounded-md shadow dark:bg-neutral-800 dark:border-neutral-700'>
+                        <div className='flex flex-col gap-2'>
+                          <h2>Titulo</h2>
+                          <input type='text' placeholder='Titulo del post' onChange={(e: any) => setContentData({ ...contentData, title: e.target.value })} value={contentData.title} className='font-light p-1.5 rounded border text-sm w-full focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                          <h2>Contenido</h2>
+                          {
+                            contentData.content.map((content, index) => (
+                              <div key={index} className='flex gap-2'>
+                                <select value={content.type} onChange={(e: any) => {
+                                  let mod: any = contentData.content
+                                  mod[index].type = e.target.value
+                                  setContentData({ ...contentData, content: mod })
+                                }} className='p-1.5 rounded border text-sm font-light focus:outline-none h-fit my-auto focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600'>
+                                  <option>Texto</option>
+                                  <option>Imagen</option>
+                                </select>
+                                {
+                                  content.type === 'Texto'
+                                    ? (
+                                      <select value={content.html} onChange={(e: any) => {
+                                        let mod: any = contentData.content
+                                        mod[index].html = e.target.value
+                                        setContentData({ ...contentData, content: mod })
+                                      }} className='p-1.5 rounded border text-sm font-light focus:outline-none h-fit my-auto focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600'>
+                                        <option>H1</option>
+                                        <option>H2</option>
+                                        <option>H3</option>
+                                        <option>H4</option>
+                                        <option>H5</option>
+                                        <option>Parrafo</option>
+                                      </select>
+                                    )
+                                    : ''
+                                }
+                                {
+                                  content.type === 'Imagen'
+                                    ? (
+                                      <input type='file' onChange={async (e: any) => {
+                                        const {data} = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/product-image-upload`, { image: e.target.files[0] }, {
+                                          headers: {
+                                            accept: 'application/json',
+                                            'Accept-Language': 'en-US,en;q=0.8',
+                                            'Content-Type': 'multipart/form-data'
+                                          }
+                                        })
+                                        let mod = contentData.content
+                                        mod[index].image = { public_id: data.image.public_id, url: data.image.url }
+                                        setContentData({ ...contentData, content: mod })
+                                      }} className='text-sm m-auto' />
+                                    )
+                                    : content.html === 'Parrafo'
+                                      ? (
+                                        <textarea placeholder='Contenido' value={content.content} onChange={(e: any) => {
+                                          let mod: any = contentData.content
+                                          mod[index].content = e.target.value
+                                          setContentData({ ...contentData, content: mod })
+                                        }} className='font-light p-1.5 rounded border text-sm w-full h-56 focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
+                                      )
+                                      : (
+                                        <input type='text' placeholder='Contenido' value={content.content} onChange={(e: any) => {
+                                          let mod: any = contentData.content
+                                          mod[index].content = e.target.value
+                                          setContentData({ ...contentData, content: mod })
+                                        }} className='font-light p-1.5 rounded border text-sm w-full focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
+                                      )
+                                }
+                              </div>
+                            ))
+                          }
+                        </div>
+                        <button onClick={(e: any) => {
+                          e.preventDefault()
+                          setContentData({ ...contentData, content: contentData.content.concat({ type: 'Texto', html: 'H1', content: '', image: { public_id: '', url: '' } }) })
+                        }} className='py-2 px-8 bg-main text-sm text-white m-auto rounded-md'>Agregar contenido</button>
+                      </div>
+                      <div className='bg-white border flex flex-col gap-4 border-white p-4 rounded-md shadow dark:bg-neutral-800 dark:border-neutral-700'>
+                        <h2>SEO</h2>
+                        <div className='flex flex-col gap-2'>
+                          <h3 className='text-sm'>Meta titulo</h3>
+                          <input type='text' placeholder='Meta titulo' onChange={(e: any) => setContentData({ ...contentData, titleSeo: e.target.value })} className='font-light p-1.5 rounded border text-sm w-full focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                          <h3 className='text-sm'>Meta descripción</h3>
+                          <textarea placeholder='Meta descripción' onChange={(e: any) => setContentData({ ...contentData, descriptionSeo: e.target.value })} className='font-light p-1.5 rounded border text-sm w-full h-32 focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
+                        </div>
+                      </div>
+                    </div>
+                    <div className='w-1/3 flex flex-col gap-4'>
+                      <div className='bg-white p-4 rounded-md shadow border border-white dark:bg-neutral-800 dark:border-neutral-700'>
+                        <h2 className='mb-4'>Visibilidad del producto</h2>
+                        <select onChange={(e: any) => {
+                          setContentData({...contentData, state: e.target.value === 'Activo' ? true : false})
+                        }} value={contentData.state ? 'Activo' : 'En borrador'} className='p-1.5 mb-2 rounded border text-sm font-light focus:outline-none w-full focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600'>
+                          <option>Activo</option>
+                          <option>En borrador</option>
                         </select>
+                      </div>
+                      <div className='bg-white p-4 rounded-md shadow border flex flex-col gap-4 border-white dark:bg-neutral-800 dark:border-neutral-700'>
+                        <h2>Imagen principal</h2>
+                        <input type='file' className='text-sm' onChange={imageChange} />
                         {
-                          content.type === 'Texto'
-                            ? (
-                              <select value={content.html} onChange={(e: any) => {
-                                let mod: any = contentData.content
-                                mod[index].html = e.target.value
-                                setContentData({ ...contentData, content: mod })
-                              }} className='p-1.5 rounded border text-sm font-light focus:outline-none h-fit my-auto focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600'>
-                                <option>H1</option>
-                                <option>H2</option>
-                                <option>H3</option>
-                                <option>H4</option>
-                                <option>H5</option>
-                                <option>Parrafo</option>
-                              </select>
-                            )
+                          contentData.image.url !== ''
+                            ? <img src={contentData.image.url} />
                             : ''
                         }
-                        {
-                          content.type === 'Imagen'
-                            ? (
-                              <input type='file' onChange={async (e: any) => {
-                                const {data} = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/product-image-upload`, { image: e.target.files[0] }, {
-                                  headers: {
-                                    accept: 'application/json',
-                                    'Accept-Language': 'en-US,en;q=0.8',
-                                    'Content-Type': 'multipart/form-data'
-                                  }
-                                })
-                                let mod = contentData.content
-                                mod[index].image = { public_id: data.image.public_id, url: data.image.url }
-                                setContentData({ ...contentData, content: mod })
-                              }} className='text-sm m-auto' />
-                            )
-                            : content.html === 'Parrafo'
-                              ? (
-                                <textarea placeholder='Contenido' value={content.content} onChange={(e: any) => {
-                                  let mod: any = contentData.content
-                                  mod[index].content = e.target.value
-                                  setContentData({ ...contentData, content: mod })
-                                }} className='font-light p-1.5 rounded border text-sm w-full h-56 focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
-                              )
-                              : (
-                                <input type='text' placeholder='Contenido' value={content.content} onChange={(e: any) => {
-                                  let mod: any = contentData.content
-                                  mod[index].content = e.target.value
-                                  setContentData({ ...contentData, content: mod })
-                                }} className='font-light p-1.5 rounded border text-sm w-full focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
-                              )
-                        }
                       </div>
-                    ))
-                  }
+                    </div>
+                  </form>
                 </div>
-                <button onClick={(e: any) => {
-                  e.preventDefault()
-                  setContentData({ ...contentData, content: contentData.content.concat({ type: 'Texto', html: 'H1', content: '', image: { public_id: '', url: '' } }) })
-                }} className='py-2 px-8 bg-main text-sm text-white m-auto rounded-md'>Agregar contenido</button>
-              </div>
-              <div className='bg-white border flex flex-col gap-4 border-white p-4 rounded-md shadow dark:bg-neutral-800 dark:border-neutral-700'>
-                <h2>SEO</h2>
-                <div className='flex flex-col gap-2'>
-                  <h3 className='text-sm'>Meta titulo</h3>
-                  <input type='text' placeholder='Meta titulo' onChange={(e: any) => setContentData({ ...contentData, titleSeo: e.target.value })} className='font-light p-1.5 rounded border text-sm w-full focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
-                </div>
-                <div className='flex flex-col gap-2'>
-                  <h3 className='text-sm'>Meta descripción</h3>
-                  <textarea placeholder='Meta descripción' onChange={(e: any) => setContentData({ ...contentData, descriptionSeo: e.target.value })} className='font-light p-1.5 rounded border text-sm w-full h-32 focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' />
+              )
+            : (
+              <div className="flex w-full mt-20">
+                <div className="m-auto mt-16 mb-16">
+                  <Spinner />
                 </div>
               </div>
-            </div>
-            <div className='w-1/3 flex flex-col gap-4'>
-              <div className='bg-white p-4 rounded-md shadow border border-white dark:bg-neutral-800 dark:border-neutral-700'>
-                <h2 className='mb-4'>Visibilidad del producto</h2>
-                <select onChange={(e: any) => {
-                  setContentData({...contentData, state: e.target.value === 'Activo' ? true : false})
-                }} value={contentData.state ? 'Activo' : 'En borrador'} className='p-1.5 mb-2 rounded border text-sm font-light focus:outline-none w-full focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600'>
-                  <option>Activo</option>
-                  <option>En borrador</option>
-                </select>
-              </div>
-              <div className='bg-white p-4 rounded-md shadow border flex flex-col gap-4 border-white dark:bg-neutral-800 dark:border-neutral-700'>
-                <h2>Imagen principal</h2>
-                <input type='file' className='text-sm' onChange={imageChange} />
-                {
-                  contentData.image.url !== ''
-                    ? <img src={contentData.image.url} />
-                    : ''
-                }
-              </div>
-            </div>
-          </form>
-        </div>
+            )
+        }
       </LeftMenu>
     </>
   )
