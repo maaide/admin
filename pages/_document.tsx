4f98cfdcc64beccf265f1b00ddd8f71c6f5755b1
Jorge Tapia <admin@maaide.com>
2023-08-08 15:58:56 -0400

    actualization general

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
       </Head>
