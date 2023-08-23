67882c317cd0e4116e0fb6a0f5f0466b31eaaf83
Jorge Tapia <admin@maaide.com>
2023-08-23 15:54:57 -0400

    actualization genreal

--- pages/productos/nuevo-producto/index.tsx
+++ pages/productos/nuevo-producto/index.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { BiArrowBack } from 'react-icons/bi'
+import { IoCloseOutline } from 'react-icons/io5'
 
 interface Props {
   categories: ICategory[]
@@ -29,6 +30,10 @@ const NewProduct: React.FC<Props> = () => {
     variations: { nameVariation: '', variations: [{ variation: '', stock: 0 }] }
   })
   const [categories, setCategories] = useState<ICategory[]>()
+  const [quantityOffers, setQuantityOffers] = useState([{
+    quantity: undefined,
+    descount: undefined
+  }])
 
   const initial = {
     name: ''
@@ -58,7 +63,7 @@ const NewProduct: React.FC<Props> = () => {
 
   const handleSubmit = async () => {
     setSubmitLoading(true)
-    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/products`, { name: information.name, description: information.description, category: information.category, price: information.price, beforePrice: information.beforePrice, images: information.images, stock: information.stock, slug: information.slug, state: information.state, tags: information.tags, titleSeo: information.titleSeo, descriptionSeo: information.descriptionSeo, variations: information.variations, productsOffer: productsOffer, cost: information.cost })
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/products`, { name: information?.name, description: information?.description, category: information?.category, price: information?.price, beforePrice: information?.beforePrice, images: information?.images, stock: information?.stock, slug: information?.slug, state: information?.state, tags: information?.tags, titleSeo: information?.titleSeo, descriptionSeo: information?.descriptionSeo, variations: information?.variations, productsOffer: productsOffer, cost: information?.cost, quantityOffers: quantityOffers })
     router.push('/productos')
   }
 
@@ -97,6 +102,46 @@ const NewProduct: React.FC<Props> = () => {
             <div className='w-1/3 flex flex-col gap-4'>
               <Visibility information={information} setInformation={setInformation} />
               <Price information={information} setInformation={setInformation} />
+              <div className='bg-white p-4 rounded-md shadow border border-white dark:bg-neutral-800 dark:border-neutral-700'>
+                <div>
+                  <h2 className='mb-4'>Descuentos por cantidad</h2>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th className='font-normal text-sm text-left'>Cantidad</th>
+                        <th className='font-normal text-sm text-left'>Descuento</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        quantityOffers.map((offer, index) => (
+                          <tr key={index}>
+                            <td><input type='number' placeholder='Cantidad' onChange={(e: any) => {
+                              const quantity = quantityOffers
+                              quantity[index].quantity = e.target.value
+                              setQuantityOffers(quantity)
+                            }} value={offer.quantity} className='font-light w-[120px] p-1.5 rounded border text-sm focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' /></td>
+                            <td><input type='number' placeholder='%' onChange={(e: any) => {
+                              const quantity = quantityOffers
+                              quantity[index].descount = e.target.value
+                              setQuantityOffers(quantity)
+                            }} value={offer.descount} className='font-light p-1.5 w-[120px] rounded border text-sm focus:outline-none focus:border-main focus:ring-1 focus:ring-main dark:border-neutral-600' /></td>
+                            <td><button><IoCloseOutline className='text-xl' /></button></td>
+                          </tr>
+                        ))
+                      }
+                    </tbody>
+                  </table>
+                  <button onClick={(e: any) => {
+                    e.preventDefault()
+                    setQuantityOffers(quantityOffers.concat({
+                      quantity: undefined,
+                      descount: undefined
+                    }))
+                  }} className='bg-main mt-2 text-white text-sm rounded-md w-36 h-8'>Agregar fila</button>
+                </div>
+              </div>
               <CategoryProduct categories={categories} information={information} setInformation={setInformation} setNewCategory={setNewCategory} />
             </div>
           </form>
